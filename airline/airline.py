75ce8c12b6c301b9340b93f2b1a5763de6c519c5
Lawrence Lim <ll4715@nyu.edu>
2023-05-02 22:12:54 +0800

    Finished index function and search_results function. They're working

--- airline/airline.py
+++ airline/airline.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from werkzeug.exceptions import abort
 
 from airline.auth import login_required
@@ -56,19 +65,141 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route("/")
+@bp.route("/", methods=("GET", "POST"))
 def index():
-    conn = get_db()
-    cursor = conn.cursor(dictionary=True)
-    query = """
-            SELECT p.id, title, body, created, author_id, username
-            FROM post p JOIN user u ON p.author_id = u.id
-            ORDER BY created DESC
-            """
-    cursor.execute(query)
-    posts = cursor.fetchall()
-    cursor.close()
-    return render_template("airline/index.html", posts=posts)
+    if request.method == "GET":
+        conn = get_db()
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                SELECT airport_name, airport_city
+                FROM airport
+                """
+        cursor.execute(query)
+        airports = cursor.fetchall()
+        cursor.close()
+
+    if request.method == "POST":
+        conn = get_db()
+        cursor = conn.cursor(dictionary=True)
+        error = None
+
+        going_to_airport = session["going_to"] = request.form.get("going_to")
+        leaving_from_airport = session["leaving_from"] = request.form.get(
+            "leaving_from"
+        )
+        departure_date = session["departure_date"] = request.form.get("departure_date")
+        return_date = session["return_date"] = request.form.get("return_date")
+
+        if (
+            going_to_airport is None
+            or leaving_from_airport is None
+            or departure_date is None
+        ):
+            error = "One or more fields are empty. Please fill out all fields."
+        elif return_date is None:
+            return redirect(
+                url_for(
+                    "airline.search_results",
+                    going_to_airport=going_to_airport,
+                    leaving_from_airport=leaving_from_airport,
+                    departure_date=departure_date,
+                )
+            )
+        else:  # User wants a return trip
+            return redirect(
+                url_for(
+                    "airline.search_results",
+                    going_to_airport=going_to_airport,
+                    leaving_from_airport=leaving_from_airport,
+                    departure_date=departure_date,
+                    return_date=return_date,
+                )
+            )
+
+        flash(error)
+
+    return render_template("airline/index.html", airports=airports)
+
+
+@bp.route("/search", methods=("GET", "POST"))
+def search_results():
+    if request.method == "GET":
+        going_to_airport = request.args.get("going_to_airport")
+        leaving_from_airport = request.args.get("leaving_from_airport")
+        departure_date = request.args.get("departure_date")
+        return_date = request.args.get("return_date")
+
+        conn = get_db()
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                SELECT airport_name, airport_city
+                FROM airport
+                """
+        cursor.execute(query)
+        airports = cursor.fetchall()
+        cursor.close()
+
+
+        conn = get_db()
+        cursor = conn.cursor(dictionary=True)
+        error = None
+        query = """
+                SELECT *
+                FROM flight
+                JOIN airport on flight.departure_airport = airport.airport_name
+                """
+        cursor.execute(query)
+        flights = cursor.fetchall()
+        cursor.close()
+
+        return render_template(
+            "airline/results.html",
+            going_to_airport=going_to_airport,
+            leaving_from_airport=leaving_from_airport,
+            departure_date=departure_date,
+            return_date=return_date,
+            flights=flights,
+            airports=airports,
+        )
+    if request.method == "POST":
+        conn = get_db()
+        cursor = conn.cursor(dictionary=True)
+        error = None
+
+        going_to_airport = session["going_to"] = request.form.get("going_to")
+        leaving_from_airport = session["leaving_from"] = request.form.get(
+            "leaving_from"
+        )
+        departure_date = session["departure_date"] = request.form.get("departure_date")
+        return_date = session["return_date"] = request.form.get("return_date")
+
+        if (
+            going_to_airport is None
+            or leaving_from_airport is None
+            or departure_date is None
+        ):
+            error = "One or more fields are empty. Please fill out all fields."
+        elif return_date is None:
+            return redirect(
+                url_for(
+                    "airline.search_results",
+                    going_to_airport=going_to_airport,
+                    leaving_from_airport=leaving_from_airport,
+                    departure_date=departure_date,
+                )
+            )
+        else:  # User wants a return trip
+            return redirect(
+                url_for(
+                    "airline.search_results",
+                    going_to_airport=going_to_airport,
+                    leaving_from_airport=leaving_from_airport,
+                    departure_date=departure_date,
+                    return_date=return_date,
+                )
+            )
+
+        flash(error)
 
 
 @bp.route("/<int:id>/update", methods=("GET", "POST"))
