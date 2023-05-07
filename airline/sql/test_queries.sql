6d30c3580b6c0e19870262941e4bb8b9814aebad
Lawrence Lim <ll4715@nyu.edu>
2023-05-07 21:13:04 +0800

    Finished view my commission and all its features

--- airline/sql/test_queries.sql
+++ airline/sql/test_queries.sql
@@ -1,27 +1,8 @@
-SELECT DISTINCT
-  a.airport_city
-FROM
-  flight AS f
-  JOIN (
-    ticket
-    NATURAL JOIN purchases
-  ) ON ticket.flight_num = f.flight_num
-  JOIN airport AS a ON f.arrival_airport = a.airport_name
-WHERE
-  ticket.airline_name = 'Delta'
-  AND purchases.purchase_date >= (NOW() - INTERVAL 1 YEAR)
-GROUP BY
-  f.arrival_airport
-ORDER BY
-  COUNT(*) DESC
-LIMIT
-  3;
-
 SELECT
-COUNT(*) * 10 AS commission
+  COUNT(*) AS total_tickets_sold
 FROM
-purchases
+  purchases
 WHERE
-purchases.booking_agent_id = 1
-AND purchases.purchase_date > DATE_SUB(NOW(), INTERVAL 30 DAY);
+  purchases.booking_agent_id = 1
+  AND purchase_date >= DATE_SUB(NOW(), INTERVAL 30 DAY)
 
--- airline/sql/triggers.sql
+++ airline/sql/triggers.sql
@@ -1,10 +1,22 @@
-CREATE TRIGGER update_commission
-AFTER INSERT ON purchases
-FOR EACH ROW
-BEGIN
-    IF NEW.booking_agent_id IS NOT NULL THEN
-        UPDATE booking_agent AS b
-        SET commission = commission + 10
-        WHERE b.booking_agent_id = NEW.booking_agent_id;
-    END IF;
+CREATE TRIGGER update_commission AFTER
+INSERT
+  ON purchases FOR EACH ROW BEGIN IF NEW.booking_agent_id IS NOT NULL THEN
+UPDATE booking_agent AS b
+SET
+  commission = commission + 0.4 * (
+    SELECT
+      price
+    FROM
+      purchases
+      NATURAL JOIN ticket
+      NATURAL JOIN flight
+    WHERE
+      purchases.ticket_id = NEW.ticket_id
+    LIMIT
+      1
+  )
+WHERE
+  b.booking_agent_id = NEW.booking_agent_id;
+
+END IF;
 END;
